1e79cf2a2f92030aa91f5a9a2dd1d8ba1f85d6f3
elijah <elijahknsubuga@gmail.com>
2021-08-02 10:23:47 +0300

    finished with the mvp

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ from PyDictionary import PyDictionary
 dictionary = PyDictionary()
 root = Tk()
 
-root.geometry("400x400")
+root.geometry("600x600")
 
 def dict():
     meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
     synonym.config(text=dictionary.synonym(word.get()))
-    antonym.config(text=dictionary.antonym(word.get())
-    
-Label(root, text="Dictionary", font=("Helvatica 20 bold"), fg="Green").pack(pady=10)
+    antonym.config(text=dictionary.antonym(word.get()))
+
+
+Label(root, text="Dictionary", font=("Helvatica 18 bold"), fg="green").pack(pady=10)
 
 frame = Frame(root)
-Label(frame, text="Please Enter a Word", font = ("Helvatica 19 bold")).pack(side=LEFT)
+Label(frame, text="Please Enter a Word", font = ("Helvatica 16 bold")).pack(side=LEFT)
 word = Entry(frame, font = ("Helvatica 15 bold"))
 word.pack()
 
@@ -24,27 +25,24 @@ frame.pack(pady=10)
 frame1 = Frame(root)
 
 Label(frame1, text="Meaning:- ", font = ("Helvatica 10 bold")).pack(side=LEFT)
-meaning=Entry(frame1, font=("Helvatica 10 bold"))
+meaning=Label(frame1, text="", font=("Helvatica 10 bold"))
 meaning.pack()
-
 frame1.pack(pady=10)
 
 #frame2
 frame2=Frame(root)
 
 Label(frame2, text="Synonym:- ", font=("Helvatica 10 bold")).pack(side=LEFT)
-synonym=Entry(frame2, font=("Helvatica 10 bold"))
+synonym = Label(frame2, text="", font=("Helvatica 10 bold"))
 synonym.pack()
-
 frame2.pack(pady=10)
 
 #frame3
 frame3 = Frame(root)
 
-Label(frame3, text="Antonym:- :- ", font=("Helvatica 10 bold")).pack(side=LEFT)
-antonym = Label(frame3, text="Antonym", font = ("Helvatica 10 bold"))
+Label(frame3, text="Antonym:- ", font=("Helvatica 10 bold")).pack(side=LEFT)
+antonym = Label(frame3, text="", font = ("Helvatica 10 bold"))
 antonym.pack(side=LEFT)
-
 frame3.pack(pady=10)
 
 Button(root, text="Submit", font=("Helvatica 10 bold"), command=dict).pack()

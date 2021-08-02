635254c82c6bfee4e5f84c0df0ccfd87b30cbd87
elijah <elijahknsubuga@gmail.com>
2021-08-02 09:44:48 +0300

    finished with the mvp

--- pydict.py
+++ pydict.py
@@ -15,19 +15,38 @@ Label(root, text="Dictionary", font=("Helvatica 20 bold"), fg="Green").pack(pady
 
 frame = Frame(root)
 Label(frame, text="Please Enter a Word", font = ("Helvatica 19 bold")).pack(side=LEFT)
-
 word = Entry(frame, font = ("Helvatica 15 bold"))
 word.pack()
 
 frame.pack(pady=10)
 
-
+#frame1
 frame1 = Frame(root)
 
-Label(frame1, text="Please Enter a Word", font = ("Helvatica 10 bold")).pack(side=LEFT)
-
+Label(frame1, text="Meaning:- ", font = ("Helvatica 10 bold")).pack(side=LEFT)
 meaning=Entry(frame1, font=("Helvatica 10 bold"))
-
 meaning.pack()
 
-frame.pack(pady=10)
+frame1.pack(pady=10)
+
+#frame2
+frame2=Frame(root)
+
+Label(frame2, text="Synonym:- ", font=("Helvatica 10 bold")).pack(side=LEFT)
+synonym=Entry(frame2, font=("Helvatica 10 bold"))
+synonym.pack()
+
+frame2.pack(pady=10)
+
+#frame3
+frame3 = Frame(root)
+
+Label(frame3, text="Antonym:- :- ", font=("Helvatica 10 bold")).pack(side=LEFT)
+antonym = Label(frame3, text="Antonym", font = ("Helvatica 10 bold"))
+antonym.pack(side=LEFT)
+
+frame3.pack(pady=10)
+
+Button(root, text="Submit", font=("Helvatica 10 bold"), command=dict).pack()
+
+root.mainloop()

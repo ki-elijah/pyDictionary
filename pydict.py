2113944670b7845e00ee433306845eeace53555f
elijah <elijahknsubuga@gmail.com>
2021-08-02 09:35:08 +0300

    created the frame of word and meaning

--- pydict.py
+++ pydict.py
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 from tkinter import *
 from PyDictionary import PyDictionary
 
@@ -21,4 +11,23 @@ def dict():
     synonym.config(text=dictionary.synonym(word.get()))
     antonym.config(text=dictionary.antonym(word.get())
     
+Label(root, text="Dictionary", font=("Helvatica 20 bold"), fg="Green").pack(pady=10)
 
+frame = Frame(root)
+Label(frame, text="Please Enter a Word", font = ("Helvatica 19 bold")).pack(side=LEFT)
+
+word = Entry(frame, font = ("Helvatica 15 bold"))
+word.pack()
+
+frame.pack(pady=10)
+
+
+frame1 = Frame(root)
+
+Label(frame1, text="Please Enter a Word", font = ("Helvatica 10 bold")).pack(side=LEFT)
+
+meaning=Entry(frame1, font=("Helvatica 10 bold"))
+
+meaning.pack()
+
+frame.pack(pady=10)
